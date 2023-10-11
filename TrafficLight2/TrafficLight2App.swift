01775d8f54fa93be777fff73e18a70ecb4fce61b
Vadim Vanatov <79990006611@yandex.ru>
2023-10-11 22:14:01 +0300

    Initial Commit

---
+++ TrafficLight2/TrafficLight2App.swift
@@ -0,0 +1,17 @@
+//
+//  TrafficLight2App.swift
+//  TrafficLight2
+//
+//  Created by HOLY NADRUGANTIX on 11.10.2023.
+//
+
+import SwiftUI
+
+@main
+struct TrafficLight2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
